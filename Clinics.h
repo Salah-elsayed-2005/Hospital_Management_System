fc8680ee85635eda38d397c1014f24981384e95b
Salah-elsayed-2005 <118000804+Salah-elsayed-2005@users.noreply.github.com>
2024-05-17 09:49:09 +0300

    Basic Structure

--- Clinics.h
+++ Clinics.h
@@ -1,11 +1,17 @@
 //
 // Created by salah elsayed on 5/11/2024.
 //
-#include <iostream>
-using namespace std;
+
 #ifndef HOPSITAL_MANAGEMENT_SYSTEM_CLINICS_H
 #define HOPSITAL_MANAGEMENT_SYSTEM_CLINICS_H
+#include <iostream>
+#include <vector>
+#include "Person.h"
+using namespace std;
 class Clinic{
     string type;
+    vector<Patient*>waiting;// to be changed with priority queue
+    Doctor*doctor;
+public:
 };
 #endif //HOPSITAL_MANAGEMENT_SYSTEM_CLINICS_H
--- Person.h
+++ Person.h
@@ -4,14 +4,30 @@
 
 #ifndef HOPSITAL_MANAGEMENT_SYSTEM_PERSON_H
 #define HOPSITAL_MANAGEMENT_SYSTEM_PERSON_H
+#include "iostream"
+using namespace std;
 class Person{ // Abstract
+    string name;
+    int age;
+    string Id;
+public:
+    string getname();
+    string getID();
+    int    getage();
+    void setAame(string n);
+    void setId(string i);
+    void setAge(int a);
 
+    virtual void displaydata()=0;
 };
 class Patient:public Person{
-
-
+    int charges;
+public:
+    void displaydata()override;
 };
-class doctor:public Person{
-
+class Doctor:public Person{
+string clinic;
+public:
+    void displaydata()override;
 };
 #endif //HOPSITAL_MANAGEMENT_SYSTEM_PERSON_H
