56a22830c5d0ecc6f00aa6120919ad051f9dd788
Nabarawyy7 <22-101123@students.eui.edu.eg>
2024-05-22 02:31:13 +0300

    baheboko koloko

--- main.cpp
+++ main.cpp
@@ -14,11 +14,13 @@ vector<Clinic>hospitalClinics;
 void searchPatient(string id);
 void addPatient();
 void removePatient(string);
+void editPatient(string);
 void displaypatients();
 
 void searchDoctor(string id);
 void addDoctor();
 void removeDoctor(string);
+void editDoctor(string);
 void displaydoctors();
 
 void startingmenu();
@@ -27,6 +29,8 @@ void editmenu();
 void displaymenu();
 void clinicmenu();
 void creditsmenu();
+
+int checkinput(int,int,int);
 int main() {
 
     Diagnosis d[5];
@@ -101,10 +105,11 @@ int main() {
 
    // displaypatients();
     //end of patients tests
+    */
 
 
     // doctors functions test
-    vector<string> days1 = {"Monday", "Wednesday", "Friday"};
+/*    vector<string> days1 = {"Monday", "Wednesday", "Friday"};
     vector<string> days2 = {"Tuesday", "Thursday"};
     vector<string> days3 = {"Monday", "Thursday"};
     vector<string> days4 = {"Wednesday", "Saturday"};
@@ -128,9 +133,15 @@ int main() {
     removeDoctor("D002");
     searchDoctor("D003");
     searchDoctor("D004");
+    displaydoctors();
+    cout << "Enter the id of the doctor you want to edit : ";
+    string id;
+    cin >> id;
+
+    editDoctor(id);
 
     displaydoctors();
-*/
+    */
     //Nabarawy's tests ends here
 
 
@@ -151,7 +162,10 @@ int main() {
     clinic.setDoctor(doctor);
     clinic.setType("ENT");
 
-}
+} // end of main function
+
+
+
 
 void startingmenu(){
     cout<<"Welcome to Our Hospital Management System"<<endl;
@@ -159,6 +173,7 @@ void startingmenu(){
     cout<<"1-Main menu\n2-credits :";
     int choice;
     cin >> choice;
+    choice = checkinput(choice ,1,2);
     if (choice==1)
         mainmenu();
     if (choice==2)
@@ -176,6 +191,7 @@ void mainmenu(){
     cout<<"1-Edit Database\n2-Display Info\n3-Reserve a clinic :";
     int choice;
     cin >> choice;
+    choice = checkinput(choice,1,3);
     if (choice==1)
         editmenu();
     if (choice==2)
@@ -189,18 +205,37 @@ void editmenu(){
     cout<<"1-Edit Doctor Info\n2-Add Doctor\n3-Remove Doctor\n4-Edit Patient Info\n5-Add Patient\n6-Remove Patient  :";
     int choice;
     cin >> choice;
-    if (choice==1)
-        //editDoctor();
-    if (choice==2)
+    choice = checkinput(choice,1,6);
+    if (choice==1){
+        string id;
+        cout << "Enter the id of the Doctor you want to edit : ";
+        cin>> id;
+        //editDoctor(id);
+        }
+    else if (choice==2){
         addDoctor();
-    if (choice==3)
-        //removeDoctor(//put id);
-    if (choice==4)
+    }
+    else if (choice==3){
+        string id;
+        cout << "Enter the id of the Doctor you want to remove : ";
+        cin>> id;
+        removeDoctor( id);
+        }
+    else if (choice==4){
+        string id;
+        cout << "Enter the id of the patient you want to edit : ";
+        cin>> id;
         //editPatient();
-    if (choice==5)
+    }
+    else if (choice==5) {
         addPatient();
-    //if (choice==6)
-        //removePatient(put id);
+    }
+    else if (choice==6) {
+        string id;
+        cout << "Enter the id of the patient you want to remove : ";
+        cin>> id;
+        removePatient( id);
+    }
 }
 void displaymenu(){
     // Display Clinic,Doctor,Patient
@@ -234,6 +269,7 @@ void addPatient() {
     cin>>age;
     cout<<"\ngender (1 male 0 female ) : ";
     cin>>gender;
+    //Na2es diagnosis
     Patient patient;
     patient.setName(name);
     patient.setId(id);
@@ -254,6 +290,58 @@ void removePatient(string id_toberemoved){ // to be tested
     }
     cout << "patient with ID " << id_toberemoved << " not found." << endl;
 
+}
+
+
+void editPatient(string id_tobe_edited){
+    for (int i = 0; i < hospitalPatients.size() ; i++ ){
+        int choice;
+        if(id_tobe_edited == hospitalPatients[i].getId()){
+            cout << "what do you want to edit in the patient with id "<< id_tobe_edited<<endl;
+            cout << "1-Patient's name\n2-Patient's id\n3-Patient's age\n4-Patient's gender\n5-condition description\n";
+            cout <<"6-diagnosis level : ";
+            cin >> choice;
+            choice = checkinput(choice,1,6);
+            if (choice == 1){
+                string name;
+                cout << "Enter the new name of the patient : ";
+                getline(cin,name);
+                getline(cin,name); // msh btshtgal b getline wahda w m3rfsh leh
+                hospitalPatients[i].setName(name);
+            }
+            if (choice == 2){
+                string id;
+                cout << "Enter the new id of the patient : ";
+                cin >> id;
+                hospitalPatients[i].setId(id);
+            }
+            if (choice == 3){
+                int age;
+                cout << "Enter the new age of the patient : ";
+                cin >> age;
+                hospitalPatients[i].setAge(age);
+            }
+            if (choice == 4){
+                bool gen;
+                cout << "Enter the right gender of the patient (1 for male and 0 for female)  : ";
+                cin >> gen;
+                hospitalPatients[i].setGender(gen);
+            }
+            if (choice == 5){
+                string condition;
+                cout << "Enter the condition description of the patient : ";
+                getline(cin,condition);
+                getline(cin,condition); //msh btshtghal b getline wahda w m3rfsh leh
+                hospitalPatients[i].setConditionDescription(condition);
+            }
+            if(choice == 6){
+                //diagnosissssssssssssssssssss
+            }
+            cout << "Patient is edited successfully"<<endl;
+            return;
+        }
+    }
+    cout << "patient with ID " << id_tobe_edited << " not found." << endl;
 }
 
 void displaypatients(){
@@ -315,6 +403,54 @@ void removeDoctor(string id_toberemoved){ //to be tested
     cout << "Doctor with ID " << id_toberemoved << " not found." << endl;
 }
 
+void editDoctor(string id_tobe_edited){
+    int choice;
+    for(int i = 0; i < hospitalDoctors.size() ; i++ ){
+        if( id_tobe_edited == hospitalDoctors[i].getID() ){
+            cout << "What do you want to edit in the doctor with id "<<id_tobe_edited <<endl;
+            cout << "1-Doctor's name\n2-Doctor's available days\n3-appointment price\n4-clinic type\n5-Doctor's id : ";
+            cin >> choice;
+            choice = checkinput(choice,1,5);
+            if (choice == 1){
+                string name;
+                cout << "Enter the Doctor's right name: ";
+                getline(cin,name);
+                getline(cin,name); //msh btshtghal b getline wahda w m3rfsh leh
+                hospitalDoctors[i].setName(name);
+            }
+            if (choice == 2){
+                vector<string> avd;
+                cout << "Enter Doctor's available days: ";
+                //missing cin method
+                hospitalDoctors[i].setAvailableDays(avd);
+            }
+            if (choice == 3){
+                int apprice;
+                cout << "Enter the appointment price: ";
+                cin >> apprice;
+                hospitalDoctors[i].setPrice(apprice);
+            }
+            if (choice == 4){
+                string clinic;
+                cout <<"Enter the clinic type : ";
+                getline(cin,clinic);
+                getline(cin,clinic); //msh btshtghal b getline wahda w m3rfsh leh
+                hospitalDoctors[i].setClinicType(clinic);
+            }
+            if (choice == 5){
+                string id;
+                cout << "Enter the Doctor's new id : ";
+                cin >> id;
+                hospitalDoctors[i].setID(id);
+            }
+            cout << "Patient is edited successfully"<<endl;
+            return;
+        }
+
+    }
+    cout << "Doctor with ID " << id_tobe_edited << " not found." << endl;
+}
+
 void displaydoctors(){
     cout << "printing all doctors details..."<<endl;
     for (int i = 0 ; i<hospitalDoctors.size();i++){
@@ -323,4 +459,15 @@ void displaydoctors(){
     }
     cout << "This is the end of the doctors list "<<endl;
 
+}
+
+
+//function that will be used multiple times to check the input
+int checkinput(int choice,int first,int last){
+
+    while (choice < first || choice > last ){
+        cout << "Please enter a number between "<<first<< " and " <<last <<" inclusively : ";
+        cin >> choice;
+    }
+    return choice;
 }
